7bce0a6133531182ae60005871158f7b5e6436c3
Cih <angel08012000@gmail.com>
2021-05-17 23:56:29 +0800

    前端
    剩新增借閱資料的部分要處理～

--- static/assets/js/book_detail_info.js
+++ static/assets/js/book_detail_info.js
@@ -1,6 +1,7 @@
 var type = ["文學小說", "商業理財", "藝術設計", "人文史地", "社會科學", "自然科普", "心理勵志", "醫療保健", "飲食", "生活風格", "旅遊", "宗教命理", "親子教養", "童書/青少年文學", "影視偶像", "輕小說", "漫畫/圖文書", "語言學習", "考試用書", "電腦資訊", "專業/教科書/政府出版品", "參考書"];
 
 var isreturn;
+var isdelete=false; //是否能刪除，預設是不行，除非目前沒有借閱資料
 
 function start(){
     //編輯書籍內容 start
@@ -55,6 +56,10 @@ function start(){
             
             document.getElementById("show_original_info").innerHTML = content;
             //輸入框 end
+            if(response.borrow_infos.length==0){
+                isreturn = true;
+                isdelete = true;
+            }
         //編輯書籍內容 end
             
             //顯示借閱資料 start
@@ -75,7 +80,7 @@ function start(){
                     borrow_info_content += '<div class="contact-form text-center col-12 col-lg-3" style="margin-top: 17px;">';
                         borrow_info_content += '<h5>';
                         
-                        if(i==response.borrow_infos.length-1 && response.returned_time==null){
+                        if(i!=response.borrow_infos.length-1 || response.returned_time==null){
                             isreturn = true;
                                     borrow_info_content += response.borrow_infos[i].borrow_time.to;
                                     borrow_info_content += '</h5>';
@@ -84,7 +89,7 @@ function start(){
                             borrow_info_content += '<div class="contact-form text-center col-12 col-lg-3" style="margin-top: 17px;">';
                             borrow_info_content += '</div>';
                         }
-                        else{//最後一位還沒還
+                        else if(i==response.borrow_infos.length-1){//最後一位還沒還
                             isreturn = false;
                                     borrow_info_content += "應歸還時間為";
                                     borrow_info_content += response.returned_time;
@@ -201,10 +206,43 @@ function edit_book(){
 //刪除書籍
 function delete_book(){
     console.log("刪除中...");
-    var id = localStorage.getItem("book_id");
-    var myURL = "http://localhost:5000/delete_book?book_id="+id;
-    console.log("myURL: "+myURL);
-    
+    if(isdelete){
+        var id = localStorage.getItem("book_id");
+        var myURL = "http://localhost:5000/delete_book?book_id="+id;
+        console.log("myURL: "+myURL);
+
+        $.ajax({
+            url: myURL,
+            type: "GET",
+            dataType: "json",
+            contentType: 'application/json; charset=utf-8',
+            success: function(response){
+                console.log("success");
+                if(response.message == "success"){
+                    window.alert("刪除成功～");
+                    window.location.href = 'book.html';
+                }
+                else{
+                    console.log("error");
+                    window.alert("刪除失敗T^T");
+                }
+            },
+            error: function(){
+                console.log("error");
+                window.alert("編輯失敗T^T");
+            }
+        });
+    }
+    else{
+        window.alert("無法刪除，此書已被借閱過～");
+    }
+}
+
+function return_book(user_id){
+    console.log("開始還書");
+    console.log("user_id: "+user_id);
+    console.log("book_id: "+localStorage.getItem("book_id"));
+    var myURL = "http://localhost:5000/return_book?user_id="+user_id+"&book_id="+localStorage.getItem("book_id");
     $.ajax({
         url: myURL,
         type: "GET",
@@ -212,24 +250,19 @@ function delete_book(){
         contentType: 'application/json; charset=utf-8',
         success: function(response){
             console.log("success");
-            if(response.message == "success"){
-                window.alert("刪除成功～");
-                window.location.href = 'book.html';
+            if(response.message == "success"){//登入失敗
+                window.alert("歸還成功～");
+                window.location.href='book_detail_info.html';
             }
-            else{
-                console.log("error");
-                window.alert("刪除失敗T^T");
+            else{//登入成功
+                window.alert("歸還失敗～");
             }
         },
         error: function(){
             console.log("error");
-            window.alert("編輯失敗T^T");
+            window.alert("歸還失敗～");
         }
     });
-}
-
-function return_book(e){
-    console.log("開始還書");
 //    var book_id = e.getAttribute("id");
 //    console.log("拿到的id: "+book_id);
 //    localStorage.setItem('book_id', book_id);
@@ -253,6 +286,47 @@ function add_borrow_info(){
     var today = year+"-"+month+"-"+date;
     console.log("today: "+today);
     
+    var year_return = today_date.getFullYear();
+    var month_return = today_date.getMonth()+1;//js從0開始取
+    var date_return = today_date.getDate()+7;
+    
+    
+    if(date_return<10){
+        date_return = "0" + date_return;
+    }
+    else if((month_return==1 || month_return==3 || month_return==5 || month_return==7 || month_return==8 || month_return==10 || month_return==12) && date_return>31){
+        month_return += 1;
+        date_return -= 31;
+        if(date_return<10){
+            date_return = "0" + date_return;
+        }
+    }
+    else if((month_return==4 || month_return==6 || month_return==9 || month_return==11) && date_return>30){
+        month_return += 1;
+        date_return -= 30;
+        if(date_return<10){
+            date_return = "0" + date_return;
+        }
+    }
+    else if(month_return==2 && year_return%4==0 && date_return>29){
+        month_return += 1;
+        date_return -= 29;
+        if(date_return<10){
+            date_return = "0" + date_return;
+        }
+    }
+    else if(month_return==2 && year_return%4!=0 && date_return>28){
+        month_return += 1;
+        date_return -= 28;
+        if(date_return<10){
+            date_return = "0" + date_return;
+        }
+    }
+    if(month_return<10){
+        month_return = "0" + month_return;
+    }
+    var need_return = year_return+"-"+month_return+"-"+date_return;
+    
     console.log("新增借閱資料的input");
     var content = document.getElementById("borrow_info").innerHTML;
     //要幫忙設ID
@@ -268,7 +342,9 @@ function add_borrow_info(){
                 content += '</h5>';
             content += '</div>';
             content += '<div class="contact-form text-center col-12 col-lg-3" style="margin: auto;">';
-                content += '<h5>尚未歸還（不能填寫）</h5>';
+                content += '<h5>應在「';
+                content += need_return;
+                content += '」歸還</h5>';
             content += '</div>';
             content += '<div class="contact-form text-center col-12 col-lg-3" style="margin: auto;">';
                 content += '<center><button type="button" class="btn return-btn" onclick="send_borrow_info()">新增</button></center>';
@@ -281,10 +357,33 @@ function add_borrow_info(){
     }
 }
 
+//新增借閱資料（傳給後端）
 function send_borrow_info(){
     console.log("送出借閱資料到後端");
     var user_id = $("#borrow_info_user_id").val();
     console.log("user ID: "+ user_id);
+    console.log("book ID: "+localStorage.getItem("book_id"));
+    
+    var myURL = "http://localhost:5000/borrow_book?user_id="+user_id+"&book_id="+localStorage.getItem("book_id");
+    $.ajax({
+        url: myURL,
+        type: "GET",
+        dataType: "json",
+        contentType: 'application/json; charset=utf-8',
+        success: function(response){
+            console.log("success");
+            if(response.message == "success"){//登入失敗
+                window.alert("新增成功！");
+                window.location.href = "book_detail_info.html";
+            }
+            else{//登入成功
+                window.alert("新增失敗！");
+            }
+        },
+        error: function(){
+            console.log("error");
+        }
+    });
 }
 
 window.addEventListener("load", start, false);
--- static/assets/js/index.js
+++ static/assets/js/index.js
@@ -1,51 +1,60 @@
-//$(document).on('ready', function() {
-//    $(".variable").slick({
-//        dots: true,
-//        infinite: true,
-//        variableWidth: true
-//    });
-//});
+var week = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 
-$(window).load(function() {
-    start();
-    if(start()){
-        console.log("開始設定");
-        $(".variable").slick({
-            dots: true,
-            infinite: true,
-            variableWidth: true
+function set(){
+    $(".variable").slick({
+        dots: true,
+        infinite: true,
+        variableWidth: true,
+        centerMode: true,
+          centerPadding: '60px',
+          slidesToShow: 3,
+          responsive: [
+            {
+              breakpoint: 768,
+              settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 3
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                arrows: false,
+                centerMode: true,
+                centerPadding: '40px',
+                slidesToShow: 1
+              }
+            }
+          ]
         });
-    }
-    
-});
+}
 
 function start(){
     console.log("start");
-    //show_new_books
+    //顯示新書 start
     var content = "";
     $.ajax({
         url: "http://localhost:5000/show_new_book",
         type: "GET",
         dataType: "json",
         contentType: 'application/json; charset=utf-8',
+        async: false,
         success: function(response){
             console.log("success");
             for(var i=0; i<response.length; i++){
-                console.log("進來迴圈了！");
-                
-//                content += '<div><img src="../static/img/blog-img/lp2.png" style="height: 300px; width: auto;"><div class="hero-slides-content"><div class="line"></div><h2 style="color: ghostwhite">書名</h2><p style="color: ghostwhite">作者 | 種類</p></div></div>';
                 
                 content += '<div>';
                     content += '<img src="';
                     content += response[i].book_info.img;
-//                    content += '../static/img/blog-img/lp2.png';
-                    content += '" style="height: 300px; width: auto;">';
+                    content += '" style="height: auto; width: 300px; margin:auto;">';
                     content += '<div class="hero-slides-content">';
                         content += '<div class="line"></div>';
-                        content += '<h2 style="color: ghostwhite">';
+                        content += '<h2 style="color: ghostwhite; width:300px; font-size: 20px; font-weight:bold; text-align: center; padding: 10px;">';
                         content += response[i].book_info.name;
                         content += '</h2>';
-                        content += '<p style="color: ghostwhite">';
+                        content += '<p style="color: ghostwhite; text-align: center;">';
                         content += response[i].book_info.author;
                         content += ' | ';
                         content += response[i].book_info.type;
@@ -54,13 +63,48 @@ function start(){
                 content += '</div>';
             }
             document.getElementById("show_new_book").innerHTML = content;
-//            $(".variable")[0].slick.refresh();
-            return true;
+            set();
         },
         error: function(){
             console.log("error");
         }
     });
+    //顯示新書 end
+    
+    
+    //顯示圖書館資訊 start
+    $.ajax({
+        url: "http://localhost:5000/show_library",
+        type: "GET",
+        dataType: "json",
+        contentType: 'application/json; charset=utf-8',
+        success: function(response){
+            console.log("success");
+            document.getElementById("library_name").innerHTML = response.name;
+            document.getElementById("library_location").innerHTML = response.address;
+            var start="", end="";
+            for(var i=0; i<7; i++){
+                start += '<div class="contact-form text-center col-12 col-lg-1" style="margin: auto; align-items: center;">';
+                    start += '<span>';
+                    start += response.time[week[i]].substr(0, 5);
+                    start += '</span>';
+                start += '</div>';
+                
+                end += '<div class="contact-form text-center col-12 col-lg-1" style="margin: auto; align-items: center;">';
+                    end += '<span>';
+                    end += response.time[week[i]].substr(6, 5);
+                    end += '</span>';
+                end += '</div>';
+            }
+            
+            document.getElementById("library_time_start").innerHTML = start;
+            document.getElementById("library_time_end").innerHTML = end;
+        },
+        error: function(){
+            console.log("error");
+        }
+    });
+    //顯示圖書館資訊 end
 }
 
-//document.addEventListener("load", start, false);
+window.addEventListener("load", start, false);
